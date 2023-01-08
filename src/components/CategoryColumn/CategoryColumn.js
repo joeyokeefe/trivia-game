1bf5bd2933a413150354b8a7d3397ed305b55926
joseph okeefe <joeyokeefe5@gmail.com>
2023-01-08 14:19:24 -0500

    retrieved categories from API

---
+++ src/components/CategoryColumn/CategoryColumn.js
@@ -0,0 +1,12 @@
+export default function CategoryColumn(props) {
+  return (
+    <div>
+      <div>{props.title}</div>
+      <div>200: </div>
+      <div>400: </div>
+      <div>600: </div>
+      <div>800: </div>
+      <div>1000: </div>
+    </div>
+  );
+}
