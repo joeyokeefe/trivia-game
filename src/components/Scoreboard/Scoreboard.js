f2aa2032d82441a08613295f963d22a7911e558e
joseph okeefe <joeyokeefe5@gmail.com>
2023-01-19 20:52:20 -0500

    Added logic for player name change, Scoreboard, and plus/minus buttons

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -3,15 +3,42 @@ import "./Scoreboard.css";
 
 export default function Scoreboard(props) {
   const [playerName, setPlayerName] = useState(`Player ${props.i + 1}`);
+  const [playerScore, setPlayerScore] = useState(0);
+
+  function handleNameChange(input) {
+    setPlayerName(input);
+  }
+
+  function handleIncreasePlayerScore() {
+    setPlayerScore((prevPlayerScore) => prevPlayerScore + 100);
+  }
+
+  function handleDecreasePlayerScore() {
+    setPlayerScore((prevPlayerScore) => prevPlayerScore - 100);
+  }
 
   return (
     <div>
       <div className="playerCard">
-        <input type="text" value={playerName} className="nameDisplay" />
-        <input type="number" value={0} className="scoreDisplay" readOnly />
+        <input
+          type="text"
+          value={playerName}
+          className="nameDisplay"
+          onChange={(e) => handleNameChange(e.target.value)}
+        />
+        <input
+          type="number"
+          readOnly
+          value={playerScore}
+          className="scoreDisplay"
+        />
         <div className="scoreControls">
-          <div className="minus">-</div>
-          <div className="plus">+</div>
+          <div className="minus" onClick={handleDecreasePlayerScore}>
+            -
+          </div>
+          <div className="plus" onClick={handleIncreasePlayerScore}>
+            +
+          </div>
         </div>
       </div>
     </div>
